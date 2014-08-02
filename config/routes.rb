1eec4db00acb796abaf68dd048c869a94d8d42c7
tanwen <tanwen217@gmail.com>
2014-08-02 22:46:00 +0800

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+  resources :users
   root to: 'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
